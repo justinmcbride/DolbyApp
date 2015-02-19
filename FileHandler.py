3e95a7b61686ad48b07c70a6b37e38fc0f7d78a2
Justin McBride <justin@dare599z.com>
2015-02-19 01:59:38 -0800

    Added single file retrieval in FileHandler

--- FileHandler.py
+++ FileHandler.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 FILE_SUBDIR = "./files/"
 
@@ -8,3 +9,15 @@ def GetAllFiles():
 		if f.endswith(".json"):
 			files.append(f)
 	return files
+
+def DoesFileExist(filename):
+	files = GetAllFiles()
+	for f in files:
+		if f == filename:
+			return True
+	return False
+
+def GetFile(filename):
+	with open(FILE_SUBDIR + filename, "r") as f:
+		contents = f.read()
+	return json.loads(contents)
